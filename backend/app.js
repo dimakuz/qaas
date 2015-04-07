74c11f300b5fd36ffe2dd22936df59d61a7be035
Dima Kuznetsov <dkuznets@redhat.com>
2015-04-07 17:15:53 +0300

    ANother...
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- backend/app.js
+++ backend/app.js
@@ -4,22 +4,46 @@ var bodyparser = require('body-parser');
 var mongodb = require('mongodb');
 var db;
 var queue_col;
+var token_col;
+var session = require('express-session');
+var cookie = require('cookie-session');
 
 app = express();
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
-
-
-app.get('/', function (req, res) {
-    db.collection('buya').findOne({name: 'foobar'}, function (err, doc) {
-        res.send(JSON.stringify(doc));
-    });
-});
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'qaas'
+}));
 
 app.post('/create', function (req, res) {
     console.log('/create');
-    res.send('ok');
+    queue_col.insert(
+        {
+            name: req.body.name,
+            secret: req.body.secret
+        },
+        function (err, doc) {
+            res.json({status: 'ok', id: doc.ops[0]._id});
+        }
+    );
 });
+
+app.post('/login', function (req, res) {
+    queue_col.findOne({_id: req.body.id}, function (err, doc) {
+        if (doc.secret == req.body.secret) {
+            var token = 1 ; //genuuid();
+            console.log('generated token' + token);
+            token_col.insert({
+                token_id: token,
+                queue_id: req.body.id
+            });
+            res.json({status: 'ok', token_id: token});
+        } else {
+            res.json({status: 'invalid' });
+        }
+})});
 
 mongodb.MongoClient.connect(DB_URL, function (err, _db) {
     console.log('DB connected');
@@ -27,3 +51,38 @@ mongodb.MongoClient.connect(DB_URL, function (err, _db) {
     queue_col = db.collection('_queue');
     app.listen(8000);
 });
+
+
+// (name, secret) -> queue id
+function create_queue(name, secret) {
+}
+
+// -> status
+function destroy_queue(id, secret) {
+}
+
+// -> status
+function enqueue(queue_id, consumer_id) {
+}
+
+
+// -> status
+function start_proccessing(queue_id, consumer_id) {
+}
+
+// -> status
+function finish_proccessing(queue_id, consumer_id) {
+}
+
+// -> (total in queue, total processing, consumer position) 
+function get_consumer_status(queue_id, consumer_id) {
+}
+
+// -> (list of consumers)
+function get_operator_status(queue_id, consumer_id) {
+}
+
+// -> status
+function cancel_queueing(queue_id, consumer_id) {
+}
+
