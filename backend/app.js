6f802b30c3a8028a70d95fc2ce188634e3031deb
Dima Kuznetsov <dkuznets@redhat.com>
2015-04-07 16:24:56 +0300

    Add DB conn
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
+var DB_URL = "mongodb://qaas2:qum6net@209.132.178.110:27017/qaas2";
 var express = require('express');
 var bodyparser = require('body-parser');
+var mongodb = require('mongodb');
+var db;
+var queue_col;
 
 app = express();
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
+
 app.get('/', function (req, res) {
-    res.send('hello');
+    db.collection('buya').findOne({name: 'foobar'}, function (err, doc) {
+        res.send(JSON.stringify(doc));
+    });
 });
 
 app.post('/create', function (req, res) {
@@ -14,5 +21,9 @@ app.post('/create', function (req, res) {
     res.send('ok');
 });
 
-app.listen(8000);
-
+mongodb.MongoClient.connect(DB_URL, function (err, _db) {
+    console.log('DB connected');
+    db = _db;
+    queue_col = db.collection('_queue');
+    app.listen(8000);
+});
