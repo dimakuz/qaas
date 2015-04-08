0a8ec3642c5614ed97c4c57a0438e34a173f0029
Saggi Mizrahi <ficoos@gmail.com>
2015-04-08 22:40:24 +0300

    dsads

--- qaas-frontend/app/models/queue.js
+++ qaas-frontend/app/models/queue.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('String'),
   secret: DS.attr('String'),
-  subscribers: DS.hasMany('subscriber'),
+  subscribers: DS.hasMany('subscriber')
 });
--- qaas-frontend/app/routes/queues/show.js
+++ qaas-frontend/app/routes/queues/show.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    this.store.find('queue', params.id);
+    return this.store.find('queue', params.id);
     //this.store.createRecord('subscriber', {
     //  user: 1,
     //  queue: params.id,
@@ -14,10 +14,14 @@ export default Ember.Route.extend({
   actions: {
     enqueue: function() {
       var self = this;
-      this.controller.get('model').save().then(
+      var user = this.store.createRecord('user', {id: localStorage.getItem('user_id')});
+      this.store.createRecord('subscriber', {
+        user: user,
+        queue: this.controller.get('model')
+      }).save().then(
         function(queue) {
-          console.log(queue._id);
-          self.transitionTo('queues.show', queue);
+          user.deleteRecord();
+          self.reload();
         });
     }
   }
