dc1b9f520b0ce4f38ceef8a579e6c4ecdabc91cc
Saggi Mizrahi <ficoos@gmail.com>
2015-04-08 16:20:42 +0300

    dsadsa

--- qaas-frontend/app/controllers/application.js
+++ qaas-frontend/app/controllers/application.js
@@ -1,11 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  username: localStorage.getItem('unsername'),
+  username: function() {
+    localStorage.getItem('username')
+  },
   actions: {
     login: function() {
       // ... Do some login stuff ...
       this.set('username', localStorage.getItem('username'));
+    },
+    logout: function() {
+      localStorage.clear();
+      this.set('username', null);
+      this.transitionToRoute('index');
     }
   }
 });
